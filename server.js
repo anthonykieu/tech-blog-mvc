8a1c7c74fc976a3068a34946fa97394cff8a63b1
Anthony Kieu <anthonykieu@gmail.com>
2021-01-04 00:57:08 -0500

    added css

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Super secret secret',
+  rolling: true,
   cookie: {
     httpOnly: true,
     maxAge: 60000
