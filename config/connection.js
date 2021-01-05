1ed8e66a4bcd72b5c449c6c4911331fce46a84c4
Anthony Kieu <anthonykieu@gmail.com>
2021-01-05 01:30:16 -0500

    updated css

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,6 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-// create connection to our db
-// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-//   host: 'localhost',
-//   dialect: 'mysql',
-//   port: 3306
-// });
-// create connection to our db
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
